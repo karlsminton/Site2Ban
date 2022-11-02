2a0115600bd4732350ab523c088b8d6955ced4d0
karlsminton <ksm1nn0@gmail>
2022-11-02 15:54:28 +0000

    banned site content is successfully replaced with html signifying this

--- police.js
+++ police.js
@@ -1,23 +1,24 @@
-let html
+// get local file urls
+let path = browser.runtime.getURL('partial/banned.html')
+let bg = browser.runtime.getURL('images/bg.webp')
 
-let path = browser.runtime.getURL('banned.html')
+// prep request for banned html
 let req = new Request(path)
-
-fetch(req)
-  .then((res) => res.text())
-  .then((text) => html = text)
-
-console.log(path)
 
 const url = new URL(document.URL)
 
 browser.storage.local.get()
   .then(
     (items) => {
-      console.log(items)
       if (items[url.hostname] === true) {
-        document.body.style.background = 'red'
-        document.body.innerHTML = html
+        // If the site is banned; request banned partial content and replace document content
+        fetch(req)
+          .then((res) => res.text())
+          .then((html) => {
+            document.body.style.background = `url(${bg})`
+            document.body.innerHTML = html
+            document.head.innerHTML = ''
+          })
       }
     },
     (e) => {
